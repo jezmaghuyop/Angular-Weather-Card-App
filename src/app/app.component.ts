e98376a8cfaa476d24348f28628f0a3678277869
Jez Reel Maghuyop <jezreelmaghuyop@outlook.com>
2021-12-04 18:59:19 +0800

    implement routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { CurrentWeatherResponse } from './@core/models/current-weather-response.model';
-import { WeatherService } from './@core/http/weather.service';
-import { CurrentWeatherRequest } from './@core/models/current-weather-request.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
 
-  weatherResponse!: CurrentWeatherResponse;
-  city: string = 'Makati';
-
-  private subscriptions = new Subscription();
-
-
-  constructor(private _weatherService: WeatherService) {
-
-  }
-
-  ngOnInit(): void {
-    this.getCurrentWeather();
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
-
-  getCurrentWeather(): void {
-    this.subscriptions.add(
-      this._weatherService.getCurrentWeather({ city: this.city } as CurrentWeatherRequest)
-        .subscribe(response => {
-          this.weatherResponse = response;
-        })
-    );
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { WeatherCardComponent } from './@features/weather-card/weather-card.component';
+import { AboutUsComponent } from './@features/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
